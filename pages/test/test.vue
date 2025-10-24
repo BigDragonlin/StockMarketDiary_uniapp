c84a7c06c86bdb0fe61c0a988cbb65d897b48ed3
BigDragonlin <51269337+BigDragonlin@users.noreply.github.com>
2025-10-24 17:19:58 +0800

    引入折线图

---
+++ pages/test/test.vue
@@ -0,0 +1,12 @@
+<template>
+    <view>test____</view>
+    <lineChart />
+</template>
+
+<script setup lang="ts" name="test">
+    import lineChart from '../lineChart/lineChart.vue';
+</script>
+
+<style scoped>
+
+</style>
